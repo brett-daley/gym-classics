fefd4f2cccf4a44ad4b8c4eef865276c09f612ad
Brett Daley <brett-daley@users.noreply.github.com>
2020-12-08 02:59:06 -0500

    Readable output for test_value_iteration.py

--- tests/test_value_iteration.py
+++ tests/test_value_iteration.py
@@ -2,7 +2,7 @@ import unittest
 
 import gym
 
-import sutton_barto_gym
+from sutton_barto_gym.envs.abstract.gridworld import Gridworld
 from sutton_barto_gym.value_iteration import value_iteration
 
 
@@ -16,7 +16,32 @@ class TestValueIteration(unittest.TestCase):
     def _run_test(self, env_id, discount):
         env = gym.make(env_id)
         V = value_iteration(env, discount)
-        print(env_id)
-        print(env._decoder)
-        print(V)
+
+        print(env_id + ':')
+        if isinstance(env, Gridworld):
+            # Print the values in the shape of the gridworld
+            self._print_gridworld(env, V)
+        else:
+            # Just print a list of the encoded/decoded states and their values
+            for s in env.states():
+                print(s, ':', env._decode(s), ':', '{:.2f}'.format(V[s]))
         print(flush=True)
+
+    def _print_gridworld(self, env, V):
+        # First get the string length of the longest number
+        formatter = lambda v: '{:+.2f}'.format(v)
+        maxlen = 0
+        for s in env._decoded_states():
+            v = V[env._encode(s)]
+            maxlen = max(maxlen, len(formatter(v)))
+
+        # Now we can actually print the values
+        for y in reversed(range(env._dims[1])):
+            for x in range(env._dims[0]):
+                state = (x, y)
+                if not env._is_block(state):
+                    s = env._encode(state)
+                    print(formatter(V[s]).rjust(maxlen), end=' ' * 2)
+                else:
+                    print(' ' * maxlen, end=' ' * 2)
+            print()
