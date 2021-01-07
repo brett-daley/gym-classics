54d5aed1e13ce825449a8d68421ce75c87de0bbf
Brett Daley <brett-daley@users.noreply.github.com>
2021-01-07 00:32:05 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gym-classics",
+    version="0.0.0",
+    author="Brett Daley",
+    author_email="b.daley@northeastern.edu",
+    description="Classic environments for reinforcement learning and dynamic"
+                "programming, implemented in OpenAI Gym.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brett-daley/gym-classics",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
